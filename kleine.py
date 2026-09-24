e93a000c5119f90377e1cf152800ac1ff1538102
Aleksandar <nordcomp21@gmail.com>
2026-09-24 13:51:33 +0200

    Smanjena memorija Chrome-a - fix OOM

--- kleine.py
+++ kleine.py
@@ -29,6 +29,12 @@ def setup_driver():
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--single-process")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-background-networking")
+    options.add_argument("--disable-images")
+    options.add_argument("--blink-settings=imagesEnabled=false")
+    options.add_argument("--window-size=1024,768")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
     return webdriver.Chrome(options=options)
 
